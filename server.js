2ec2d78b50dfa2bdf5bfcf15aff339b9cc5ad3a9
Artem Shylai <artem_shylai@epam.com>
2017-07-28 23:00:56 +0300

    add middleware to handle static and add API

---
+++ server.js
@@ -0,0 +1,17 @@
+import config, {nodeEnv} from './config';
+import express from 'express';
+import apiRouter from './api/index'
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello Express!');
+});
+
+server.use('/api', apiRouter);
+
+server.use(express.static('public'));
+
+server.listen(config.port, () => {
+    console.info('Express listening on port', config.port);
+});
