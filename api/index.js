7a5e7e3e67d44bbe51bf0b673bdc1996228b3908
Artem Shylai <artemshilay@gmail.com>
2017-07-29 00:12:00 +0300

    add simple route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import userData from './user';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send({
         data: []
+    });
+});
+
+router.get('/user', (req, res) => {
+    res.send({
+        data: userData
     });
 });
 
