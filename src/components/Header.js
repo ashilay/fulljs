044c3962a94202e61baceff6da7fd2ebcac73b4c
Artem Shylai <artemshilay@gmail.com>
2017-08-03 00:31:25 +0300

    React components and state

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'React';
+
+const Header = ({message}) => {
+    return (
+        <h2 className="Header text-center">
+            {message}
+        </h2>
+    )
+}
+
+Header.propTypes = {
+    message: React.PropTypes.string
+}
+
+Header.defaultProps = {
+    message: " !"
+}
+
+export default Header;
